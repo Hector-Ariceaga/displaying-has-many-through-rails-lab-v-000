3196dbf3916cc70b407ce545ca319cc2842f075a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-12 17:15:11 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def to_readable
-    self.appointment_datetime = "#{self.appointment_datetime.strftime(%^B)} #{self.appointment_datetime.day}, #{self.appointment_datetime.year} at #{self.appointment_datetime.hour}:#{self.appointment_datetime.min}"
+    self.appointment_datetime = "#{self.appointment_datetime.strftime("%^B")} #{self.appointment_datetime.day}, #{self.appointment_datetime.year} at #{self.appointment_datetime.hour}:#{self.appointment_datetime.min}"
   end
 end
